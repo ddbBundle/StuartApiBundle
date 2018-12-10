fcabe22c70b168553c7b5d466288851fdc0ba324
Dennis de Best <dennis@debest.fr>
2018-12-10 20:35:25 +0100

    Try to add routes to the controller (hint: it failed fo far, xml validation error)

--- src/StuartApi.php
+++ src/StuartApi.php
@@ -2,6 +2,12 @@
 
 namespace DdB\StuartApiBundle;
 
+use Stuart\Client;
+use Stuart\Infrastructure\Authenticator;
+use Stuart\Infrastructure\Environment;
+use Stuart\Infrastructure\HttpClient;
+use Stuart\Job;
+
 class StuartApi
 {
 
@@ -9,9 +15,30 @@ class StuartApi
 
     private $publicKey;
 
+    private $client;
+
     public function __construct(string $privateKey, string $publicKey)
     {
         $this->privateKey = $privateKey;
         $this->publicKey = $publicKey;
+
+        $environment = Environment::SANDBOX;
+
+        $authenticator = new Authenticator($environment, $publicKey, $privateKey);
+
+        $this->client = new Client(new HttpClient($authenticator));
+    }
+
+    public function addSimpleJob($pickupAddress, $dropOffAddress, $packageType = 'small'){
+        $job = new Job();
+
+        $job->addPickup($pickupAddress);
+
+        $job->addDropOff($dropOffAddress)
+            ->setPackageType($packageType);
+
+        $jobOrder = $this->client->createJob($job);
+
+        return $jobOrder;
     }
 }
